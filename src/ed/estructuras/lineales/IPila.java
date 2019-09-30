addbf3079551dc525598438efe3df105fc7ac5f1
Diana Laura Nicolás Pavía <nicolasdi@ciencias.unam.mx>
2019-09-29 22:05:06 -0500

    Comentarios corregidos

--- src/ed/estructuras/lineales/IPila.java
+++ src/ed/estructuras/lineales/IPila.java
@@ -3,37 +3,41 @@
  * Se permite consultarlo para fines didácticos en forma personal,
  * pero no está permitido transferirlo tal cual a estudiantes actuales o potenciales.
  */
+
 package ed.estructuras.lineales;
 
 import java.util.Collection;
-import java.util.NoSuchElementException;
 
 /**
  * Estructura "Último en entrar, primero en salir".
- * Corresponde a una pila que no permite almacenar elementos nulos.
  * @author veronica
  */
+// El siguiente comentario estaba originalmente en la interfaz, viendo
+// las pruebas unitarias, vemos que se pide que se permita agregar
+// elemento nulos.
+///////* Corresponde a una pila que no permite
+// almacenar elementos nulos.
 public interface IPila<E> extends Collection<E> {
-    
+
     /**
      * Muestra el elemento al tope de la pila.
-	 * Devuelve <code>null</code> si está vacía.
+     * Devuelve <code>null</code> si está vacía.
      * @return Una referencia al elemento siguiente.
      */
     public E mira();
-    
+
     /**
      * Devuelve el elemento al tope de la pila y lo elimina.
-	 * Devuelve <code>null</code> si está vacía.
+     * Devuelve <code>null</code> si está vacía.
      * @return Una referencia al elemento siguiente.
      */
     public E expulsa();
-    
+
     /**
      * Agrega un elemento al tope de la pila.
      * @param e Referencia al elemento a agregar.
-	 * @throws IllegalArgumentException si se intenta agregar <code>null</code>.
      */
+    //@throws IllegalArgumentException si se intenta agregar <code>null</code>.
     public void empuja(E e);
-    
+
 }
